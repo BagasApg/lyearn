27236ab12fb20f9a66c885e46753abcfc573a1a2
BagasApg <bagasapg@gmail.com>
2024-11-22 12:46:03 +0700

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lyearn</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="bg-blue-500 text-white p-4">
+        TailwindCSS is working!
+    </div>
+    <button class="btn">Tailwind</button>
+</body>
+@vite('resources/js/app.js')
+</html>
