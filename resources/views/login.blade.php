bb119b93cec2091c46d142e3e278a1ebe5390bd3
BagasApg <bagasapg@gmail.com>
2024-11-26 08:07:54 +0700

    little progress to login, TODO: finish login and register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="">
-        <div class="card w-80 glass">
+        <div class="card w-[400px] glass">
 
             <div class="card-body">
                 <div class="login-title flex justify-center items-center gap-2 pb-1">
@@ -26,18 +26,6 @@
                         class="glass px-3 py-1 rounded-md focus:shadow-[0_0_0.25rem_0.05rem_rgba(17,24,39,0.1)] border-gray-900 focus:outline-none focus:ring-1 focus:ring-base-100">
 
                 </div>
-                <label class="form-control w-full max-w-xs">
-                    <div class="label">
-                        <span class="label-text">What is your name?</span>
-                    </div>
-                    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="h-4 w-4 opacity-70">
-                        <path fill-rule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </label>
                 <div class="login-button"></div>
             </div>
         </div>
